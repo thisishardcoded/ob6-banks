ed60461346c717fa00b2b4cb43daa055071ceed5
Jim Harris <hardcoded@me.com>
2020-12-01 15:56:33 +0000

    Update bank.py

--- bank.py
+++ bank.py
@@ -4,7 +4,10 @@ sysex_file = sys.argv[1]
 i = 0
 pos = 4
 old_bank = -1
-new_bank = sys.argv[2]
+new_bank = int(sys.argv[2])
+
+if(new_bank < 0 or new_bank > 4):
+    raise Exception("New bank must be between 0 and 4")
 
 with open(sysex_file, "rb") as f:
     patch_data = list(f.read())
@@ -13,7 +16,7 @@ with open(sysex_file, "rb") as f:
             this_bank = byte
             if(not(old_bank > -1 and this_bank != old_bank)):
                 old_bank = this_bank
-            patch_data[pos] = int(new_bank)
+            patch_data[pos] = new_bank
             pos = pos + 1178
         i = i + 1
 with open(sysex_file, "wb") as f:
